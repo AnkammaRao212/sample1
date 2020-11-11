1fc91385010685a2d4592820e3185d2c5a263d95
AnkammaRao212 <65533900+AnkammaRao212@users.noreply.github.com>
2020-11-11 16:34:20 +0530

    Update App.java

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "WELCOME TO DEVOPS" );
+        System.out.println( "WELCOME TO jenkis with DEVOPS" );
     }
 }
